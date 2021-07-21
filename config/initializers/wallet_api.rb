d5dfedc2e43148dfe2bdde0a1d8c6a7c9341c95a
Hữu Hà <business@zsmart.tech>
2021-07-21 05:07:41 +0000

    Fixed gas

--- config/initializers/wallet_api.rb
+++ config/initializers/wallet_api.rb
@@ -1,5 +1,7 @@
 Peatio::Wallet.registry[:bitcoind] = Bitcoin::Wallet
-Peatio::Wallet.registry[:geth] = Ethereum::Wallet
-Peatio::Wallet.registry[:parity] = Ethereum::Wallet
+Peatio::Wallet.registry[:geth] = Ethereum::Eth::Wallet
+Peatio::Wallet.registry[:parity] = Ethereum::Eth::Wallet
+Peatio::Wallet.registry[:"geth-bsc"] = Ethereum::Bsc::Wallet
+Peatio::Wallet.registry[:"geth-heco"] = Ethereum::Heco::Wallet
 Peatio::Wallet.registry[:opendax_cloud] = OpendaxCloud::Wallet
 Peatio::Wallet.registry[:old_bitcoin] = OldBitcoin::Wallet
---
+++ lib/peatio/ethereum/eth/params.rb
@@ -0,0 +1,15 @@
+module Ethereum::Heco
+  module Params
+    def native_currency_id
+      'eth'
+    end
+
+    def coin_type
+      'eth'
+    end
+
+    def token_name
+      'erc20'
+    end
+  end
+end
