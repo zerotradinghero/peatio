240f5ff38bb462f7859237f7ab31e631364f457a
Hữu Hà ZSmart <huuhadz2k@gmail.com>
2022-01-18 17:32:52 +0700

    Fix http client

--- config/initializers/http_client_extends.rb
+++ config/initializers/http_client_extends.rb
@@ -1,21 +1,8 @@
 require 'json'
 require 'faraday'
-require 'faraday_middleware'
 
-# Create HTTP request with ruby Faraday client library
-module Faraday
-  class Connection
-    alias original_run_request run_request
-    def run_request(method, url, body, headers, &block)
-      original_run_request(method, url, body, headers, &block).tap do |response|
-        response.env.instance_variable_set :@request_body, body if body
-      end
-    end
-  end
-end
-
-def http_client
-  Faraday.new(url: @root_api_url) do |conn|
+def http_client(url)
+  Faraday.new(url: url) do |conn|
     conn.request :json
     conn.response :json
     conn.adapter Faraday.default_adapter
@@ -23,6 +10,6 @@ def http_client
 end
 
 # Example
-# http_client
-#   .public_send(:post,'http://barong:8001/api/v2/management/phones/send', generate_jwt_management({ :uid => 'UID123', :content => 'Hello World'}))
+# http_client('http://barong:8001/api/v2/management')
+#   .public_send(:post, '/phones/send', generate_jwt_management({ :uid => 'UID123', :content => 'Hello World'}))
 #   .body
